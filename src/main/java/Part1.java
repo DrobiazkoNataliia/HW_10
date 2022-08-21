a261a9602dc1cb0b0fe5bc104554ea8b668be007
Drobiazko Nataliia <nat123estre@gmail.com>
2022-08-21 11:55:59 +0300

    Update Part1.java

--- src/main/java/Part1.java
+++ src/main/java/Part1.java
@@ -1,40 +1,31 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part1 {
-
     public static void main(String[] args) throws IOException {
-        FileInputStream fileInputStream = new FileInputStream("./src/main/resources/file.txt");
-        InputStreamReader input = new InputStreamReader(fileInputStream);
-        BufferedReader br = new BufferedReader(input);
-        StringBuilder sb = new StringBuilder();
-        String line = br.readLine();
-        while (line != null) {
-            sb.append(line);
-            line = br.readLine();
-        }
+        try (BufferedReader br = new BufferedReader(new FileReader("./src/main/resources/file.txt"))) {
+            String line;
+            String str = null;
 
-        Pattern pattern1 = Pattern.compile("(\\(\\d{3}\\)\\s\\d{3}-\\d{4})");
-        Pattern pattern2 = Pattern.compile("(\\d{3}-\\d{3}-\\d{4})");
-        List<Pattern> patterns = new ArrayList<>();
-        patterns.add(pattern1);
-        patterns.add(pattern2);
-        for (Pattern pattern: patterns) {
-            Matcher matcher = pattern.matcher(sb);
-            if (matcher.find()) {
-                System.out.println(matcher.group(0));
+            ArrayList<String> array = new ArrayList<>();
+
+            while ((line = br.readLine()) != null) {
+                str = new String(line);
+                array.add(str);
+            }
+
+            Pattern p = Pattern.compile("(\\(\\d{3}\\)\\s\\d{3}-\\d{4})|(\\d{3}-\\d{3}-\\d{4})");
+            boolean found = false;
+            for (String s : array) {
+                if (p.matcher(s).find()) {
+                    found = true;
+                    System.out.println(s);
+                }
             }
         }
-
-        fileInputStream.close();
-        input.close();
-        br.close();
     }
 }
-
 
 
 
